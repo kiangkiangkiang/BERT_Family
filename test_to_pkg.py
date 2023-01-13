e6178b5f5baea75b8a806110b991493464e864ea
Luka Jiang <lukajiang1998@gmail.com>
2023-01-13 08:07:08 +0000

    aa

--- test_to_pkg.py
+++ test_to_pkg.py
@@ -8,7 +8,7 @@ from transformers import BertForSequenceClassification, BertForQuestionAnswering
 from tqdm.auto import tqdm
 import json
 from datasets import load_dataset, dataset_dict
-from typing import List, Optional, Iterable, Any, Union
+from typing import List, Optional, Iterable, Any, Union, Dict
 import numpy as np
 ########## Modules ##########
 #One or two sentence with one dim label
@@ -27,10 +27,12 @@ class BERTFamily(nn.Module):
         self.pretrained_model = pretrained_model
         self.tokenizer = BertTokenizerFast.from_pretrained(pretrained_model)
         self.max_length = max_length
-        self.train_data_loader, self.dev_data_loader, self.test_data_loader =  None, None, None
+        self.train_data_loader, self.validation_data_loader, self.test_data_loader =  None, None, None
         self.model, self.batch_size = None, 100
         self.status = {"BERT_Type": ["BERTFamily"],
-                        "hasData\t": False,
+                        "has_train_data": False,
+                        "has_validation_data": False,
+                        "has_test_data": False,
                         "hasModel": False,
                         "isTrained": False,
                         "accumulateEpoch": 0}
@@ -142,7 +144,7 @@ class BFClassification(BERTFamily):
         Input:
         raw_data: n by p, n: observations (total sequence). p: number of sequences in each case.
         raw_target: a list of n-length.
-        type: train, dev, test, other
+        type: train, validation, test, other
         **kwargs: The argument in DataLoader
 
         Return 3 object:
@@ -154,10 +156,10 @@ class BFClassification(BERTFamily):
         tmp_dataset = self.ClassificationDataset(raw_data=raw_data, raw_target=raw_target, 
                                                  tokenizer=tokenizer, max_length=self.max_length)
 
-        if data_type in ["train", "test", "dev"]:     
+        if data_type in ["train", "test", "validation"]:     
             exec("self." + data_type + "_data_loader" + " = DataLoader(tmp_dataset, batch_size=batch_size, shuffle=True, pin_memory=True, **kwargs)")
             self.batch_size = batch_size
-            self.status["hasData\t"] = True
+            self.status["has_"+data_type+"_data"] = True
             self.label_length = len(tmp_dataset.raw_target_dict)
             return eval("self." + data_type + "_data_loader")
 
@@ -194,11 +196,11 @@ class BFClassification(BERTFamily):
                 predictions = pred if predictions is None else torch.cat((predictions, pred))
         return list(predictions)
 
-    #要改
+
     def train(
         self, 
         train_data_loader: DataLoader, 
-        dev_data_loader: Optional[DataLoader]=None, 
+        validation_data_loader: Optional[DataLoader]=None, 
         epochs: int=5, 
         optimizer: Any=None, 
         lr_scheduler: Any=None,
@@ -213,59 +215,55 @@ class BFClassification(BERTFamily):
         self.status["isTrained"] = True
         
         print("Start training ...")
-        #self.model.to(self.device)
+        self.model.to(self.device)
         self.model.train()
         for epoch in range(epochs):
             total, correct, running_loss = 0, 0, 0
             for df in tqdm(train_data_loader):
                 optimizer.zero_grad()
-                input = [t for t in df if t is not None]
-                outputs = self.model(input_ids=input[0]["input_ids"].squeeze(1).to(self.device), 
-                                token_type_ids=input[0]["token_type_ids"].squeeze(1).to(self.device), 
-                                attention_mask=input[0]["attention_mask"].squeeze(1).to(self.device), 
-                                labels = input[1].to(self.device))
+                outputs = self.model(input_ids=df[0]["input_ids"].squeeze(1).to(self.device), 
+                                token_type_ids=df[0]["token_type_ids"].squeeze(1).to(self.device), 
+                                attention_mask=df[0]["attention_mask"].squeeze(1).to(self.device), 
+                                labels = df[1].to(self.device))
                 outputs[0].backward()
                 optimizer.step()
                 running_loss += outputs[0].item()
-                _, pred = torch.max(outputs[1], -1)
+                pred = torch.max(outputs[1], -1).indices.to("cpu")
 
-                total += input[1].size(0)
-                correct += (pred ==input[1]).sum().item()
+                total += df[1].size(0)
+                correct += (pred == df[1]).sum().item()
 
             lr_scheduler.step()
-            if eval & (dev_data_loader is not None):
-                self.model.eval()
-                _, evalAcc, evalLoss = self.test(self.model, dev_data_loader, eval=True)
-                self.model.train()
-                print('[epoch %d] loss: %.3f, TrainACC: %.3f, EvalACC: %.3f, EvalLoss: %.3f' %(epoch + 1, running_loss, correct/total, evalAcc, evalLoss))
+            if eval & (validation_data_loader is not None):
+                eval_result = self.evaluation(self.model, validation_data_loader, eval=True)
+                print('[epoch %d] TrainACC: %.3f, loss: %.3f, EvalACC: %.3f, EvalLoss: %.3f' %(epoch + 1, correct/total, running_loss, eval_result["Accurancy"], eval_result["Loss"]))
             else:
-                print('[epoch %d] loss: %.3f, ACC: %.3f' %(epoch + 1, running_loss, correct/total))
+                print('[epoch %d] ACC: %.3f, loss: %.3f' %(epoch + 1, correct/total, running_loss))
             self.status["accumulateEpoch"] += 1
 
-    #這個也要再改 （感覺是大改）
-    def test(
+
+    def evaluation(
         self, 
         model:BertForSequenceClassification,
-        data_loader:DataLoader,
+        eval_data_loader:DataLoader,
         eval: bool=False
-        ) -> tuple[list, float, int]:
+        ) -> Dict[str, float]:
 
-        predictions = None
+        model.eval()
         total, correct, loss= 0, 0, 0
         with torch.no_grad():
-            for df in data_loader if eval else tqdm(data_loader):                
-                input = [t for t in df if t is not None]        
-                outputs = model(input_ids=input[0]["input_ids"].squeeze(1).to(self.device), 
-                                token_type_ids=input[0]["token_type_ids"].squeeze(1).to(self.device), 
-                                attention_mask=input[0]["attention_mask"].squeeze(1).to(self.device))
-                logits = outputs[0]
-                _, pred = torch.max(logits.data, -1)
-                labels = input[1].to(self.device)
-                total += labels.size(0)
-                correct += (pred == labels).sum().item()
-                predictions = pred if predictions is None else torch.cat((predictions, pred))
+            for df in eval_data_loader if eval else tqdm(eval_data_loader):                
+                outputs = model(input_ids=df[0]["input_ids"].squeeze(1).to(self.device), 
+                                token_type_ids=df[0]["token_type_ids"].squeeze(1).to(self.device), 
+                                attention_mask=df[0]["attention_mask"].squeeze(1).to(self.device),
+                                labels=df[1].to(self.device))
+                loss += outputs[0].item()
+                pred = torch.max(outputs[1].data, -1).indices.to("cpu")
+                total += df[1].size(0)
+                correct += (pred == df[1]).sum().item()
         acc = float(correct / total)
-        return list(predictions), acc, loss
+        model.train()
+        return {"Accurancy": acc, "Loss": loss}
 
 
 
@@ -283,7 +281,7 @@ def auto_build_model(
 
     if dataset:
         assert (dataset_x_features is not None) & (dataset_y_features is not None), "Missing x, y features name."
-        (x_dataframe, y), data_type = load_dataset_dict(data=dataset, x=dataset_x_features, y=dataset_y_features, data_type=data_type)
+        x_dataframe, y, data_type = load_dataset_dict(data=dataset, x=dataset_x_features, y=dataset_y_features, data_type=data_type)
 
     pretrained_model = "bert-base-uncased" if not pretrained_model else pretrained_model
     result_model = BFClassification(pretrained_model=pretrained_model, max_length=max_length)
@@ -316,17 +314,16 @@ def load_dataset_dict(
     assert len(y) == 1, "The dimension of y is not 1 (multilvariable task is not applied now)."
     if type(data).__name__ == "DatasetDict":
         dataset_dict = sorted(data.values(), key=len, reverse=True)
-        data_type = ["train", "test", "dev"]
-        result_x = result_y = result_type = []
-        count = 0
-        for dataset in dataset_dict:
+        data_type = ["train", "test", "validation"]
+        result_x, result_y, result_type = [], [], []
+        for count, dataset in enumerate(dataset_dict):
             tmpx, tmpy = dataset2dataframe(data=dataset, x=x, y=y)
             result_x.append(tmpx); result_y.append(tmpy); result_type.append(data_type[count])
-            count += 1
-            if count == 3: 
+            if count == 2: 
                 return result_x, result_y, result_type
     elif type(data).__name__ == "Dataset":
-        return dataset2dataframe(data=data, x=x, y=y), data_type
+        tmpx, tmpy = dataset2dataframe(data=data, x=x, y=y)
+        return tmpx, tmpy, data_type
     else:
         raise AttributeError(type(data).__name__ + " type data cannot be handled. Please input a 'Dataset' or 'DatasetDict' type data.") 
 
@@ -380,9 +377,15 @@ import gc
 #test
 dataset = load_dataset('glue', 'mrpc')
 dataset
-mymodel = auto_build_model(dataset=dataset["train"], 
+mymodel = auto_build_model(dataset=dataset, 
                            dataset_x_features=['sentence1', 'sentence2'],
                            dataset_y_features=["label"],
-                           batch_size=128)
+                           batch_size=100)
 gc.collect()
-mymodel.train(train_data_loader=mymodel.train_data_loader, epochs=1)
+mymodel.show_status()
+
+mymodel.train(train_data_loader=mymodel.train_data_loader, 
+              validation_data_loader=mymodel.validation_data_loader, epochs=1,
+              eval=True)
+
+mymodel.evaluation(model=mymodel.model, data_loader=mymodel.test_data_loader, eval=False)
