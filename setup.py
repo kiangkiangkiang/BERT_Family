cc403d907e9dd90af90fb2ed4f81ea734ff42240
Bo Syue  Jiang <lukajiang1998@gmail.com>
2023-02-01 11:19:39 +0800

    test

--- setup.py
+++ setup.py
@@ -9,7 +9,10 @@ author_email='lukajiang1998@gmail.com',
 url='https://github.com/kiangkiangkiang/BERT_Family',
 packages=find_packages(),
 # 表明當前模塊依賴哪些包，若環境中沒有，則會從pypi中下載安裝  
-install_requires=['docutils>=0.3'],  
+install_requires=['transformers >= 4.25.1',
+                    'torch >= 1.13.1',
+                    'pandas >= 1.5.2',
+                    'tqdm >= 6.2]'],  
 classifiers = [  
         # 發展時期,常見的如下  
         # 詳見https://pypi.org/pypi?%3Aaction=list_classifiers
